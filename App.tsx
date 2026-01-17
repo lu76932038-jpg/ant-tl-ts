ba7fb1399502c8807ca15d8760013373d316afdb
lu76932038-jpg <lu76932038@gmail.com>
2026-01-18 00:56:50 +0800

    fix: 多项目登录页面修复参数

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ import ProductDetail from './pages/stock/ProductDetail';
 const App: React.FC = () => {
     return (
         <AuthProvider>
-            <BrowserRouter basename="/ant-tool">
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <Routes>
                     {/* Public Routes */}
                     <Route path="/login" element={<Login />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ant-tool/',
+    base: env.VITE_BASE_PATH,
     server: {
       port: parseInt(env.VITE_PORT),
       host: '0.0.0.0',
